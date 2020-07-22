58f63b3f11f7c8da45102d41502cfa09815af1d1
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2020-07-22 01:07:46 -0600

    terminado

--- Cap1/Ejercicios/04.py
+++ Cap1/Ejercicios/04.py
@@ -4,14 +4,14 @@ from sympy import *
 z1=1+I
 z2=-2+I
 z3=-I
-#z1+z2+z3
+#a) z1+z2+z3
 print(f"z1+z2+z3={z1+z2+z3}")
-#z1z2/z3
+#b) z1z2/z3
 print(f"z1z2/z3={(z1*z2)/z3}")
-#e^z1
+#c) e^z1
 print(f"e^z1={exp(z1).expand(complex=True)}")
-#ln(z1)
+#d) ln(z1)
 print(f"ln(z1)={log(z1)}")
 pprint(ln(z1))
-#sin(z3)
+#e) sin(z3)
 print(f"sin(z3)={sin(z3)}")
