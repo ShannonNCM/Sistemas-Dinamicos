ad42d76d4277ccb548a151048f057e3fad87a7ed
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2020-07-22 01:08:26 -0600

    terminado

--- Cap1/Ejercicios/05.py
+++ Cap1/Ejercicios/05.py
@@ -1,13 +1,13 @@
 #Evaluar los limmites
 from sympy import *
 x=symbols('x')
-#a
+#a)
 print(limit(sin(x)/x,x,0))
-#b
+#b)
 print(limit(((x**3)+(3*(x**2))-5)/((2*(x**3))-(7*x)),x,oo))
-#c
+#c)
 print(limit((cos(x)+1)/(x-pi),x,pi))
-#d
+#d)
 print(limit(1/x,x,0,'+'))
-#e
+#e)
 print(limit(((2*sinh(x))-(2*sin(x)))/(cos(x)-1),x,0))
