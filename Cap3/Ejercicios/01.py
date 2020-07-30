f6418fc9be1cf56d78da8bed64c17136f8967cef
ShannonNCM <shannyotaku@gmail.com>
2020-07-29 22:54:55 -0600

    nueva version

--- Cap3/Ejercicios/01.py
+++ Cap3/Ejercicios/01.py
@@ -1,2 +1,40 @@
 '''a) hallar los eigenvalores y eigenvectores de la matriz'''
 
+#librerias a utilizar
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+import pylab as pl
+
+#sistema
+def dx_dt(x,t):
+    return [0*x[0]+x[1],x[0]-x[1]+x[0]**3]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=Y
+v=X-Y+X**3
+pl.quiver(X,Y,u,v,color='b')
+
+plt.show()
