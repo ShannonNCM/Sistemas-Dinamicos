13e0e062b576c7682f5f3441a89001a5d4d5753d
ShannonNCM <shannyotaku@gmail.com>
2020-07-29 23:19:20 -0600

    nueva version

--- Cap3/Ejercicios/04.py
+++ Cap3/Ejercicios/04.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy.integrate import odeint
 import pylab as pl
 
+#a)
 #sistema
 def dx_dt(x,t):
     return [0*x[0]+x[1],x[0]-x[1]+x[0]**3]
@@ -37,4 +38,38 @@ u=Y
 v=X-Y+X**3
 pl.quiver(X,Y,u,v,color='b')
 
+plt.show()
+
+
+#b)
+#sistema
+def dx_dt(x,t):
+    return [-2*x[0]-x[1]+2,x[0]*x[1]]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=-2*X-Y+2
+v=X*Y
+pl.quiver(X,Y,u,v,color='b')
 plt.show()
