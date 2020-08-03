8320bd393d9c7af901f70007a70a0af2bca9ccfe
ShannonNCM <shannyotaku@gmail.com>
2020-08-03 00:49:34 -0600

    'terminado'

--- Cap3/Ejercicios/04.py
+++ Cap3/Ejercicios/04.py
@@ -112,3 +112,141 @@ plt.show()
 
 
 #d)
+#sistema
+def dx_dt(x,t):
+    return [2-x[0]-x[1]**2,-x[1]*(x[0]**2*x[1]**2-3*x[0]+1)]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=2-X-Y**2
+v=-Y*(X**2+Y**2-3*X+1)
+pl.quiver(X,Y,u,v,color='b')
+plt.show()
+
+
+
+#e)
+#sistema
+def dx_dt(x,t):
+    return [x[1]**2,x[0]**2]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=Y**2
+v=X**2
+pl.quiver(X,Y,u,v,color='b')
+plt.show()
+
+
+
+#f)
+#sistema
+def dx_dt(x,t):
+    return [x[0]**2,x[1]**2]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=X**2
+v=Y**2
+pl.quiver(X,Y,u,v,color='b')
+plt.show()
+
+
+
+#g)
+#sistema
+def dx_dt(x,t):
+    return [x[1],x[0]**3]
+#trayectorias en tiempo hacia adelante
+ts=np.linspace(0,10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#trayectorias en tiempo hacia atras
+ts=np.linspace(0,-10,500)
+ic=np.linspace(-3,3,6)
+for r in ic:
+    for s in ic:
+        x0=[r,s]
+        xs=odeint(dx_dt,x0,ts)
+        plt.plot(xs[:,0],xs[:,1],"r-")
+#etiquetas de ejes y estilo de letra
+plt.xlabel('x',fontsize=10)
+plt.ylabel('y',fontsize=10)
+plt.tick_params(labelsize=10)
+plt.xlim(-3,3)
+plt.ylim(-3,3)
+#campo vectorial
+X,Y=np.mgrid[-3:3:20j,-3:3:20j]
+u=Y
+v=X**3
+pl.quiver(X,Y,u,v,color='b')
+plt.show()
+
+
